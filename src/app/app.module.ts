a8bc91db3c71cb2527a8e0903a8e6ce9f7840029
Justin Stonecipher <justin.stonecipher11@gmail.com>
2021-07-15 22:38:52 -0600

    add new footer/navbar and other updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {enableProdMode} from '@angular/core';
-import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { enableProdMode } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -13,11 +13,30 @@ import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { TryoutGuideComponent } from './components/tryout-guide/tryout-guide.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
 import { TryoutDialogComponent } from './components/tryout-dialog/tryout-dialog.component';
 import { CvcDialogComponent } from './components/cvc-dialog/cvc-dialog.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+// import { 
+//   MatToolbarModule,
+//   MatButtonModule,
+//   MatMenuModule,
+//   MatCardModule,
+//   MatIconModule,
+//   MatListModule,
+//   MatGridListModule,
+//   MatDividerModule,
+//   MatDialogModule,
+// } from '@angular/material';
 
 enableProdMode();
 
@@ -29,6 +48,8 @@ enableProdMode();
     TryoutGuideComponent,
     TryoutDialogComponent,
     CvcDialogComponent,
+    FooterComponent,
+    TopNavBarComponent
   ],
   imports: [
     BrowserModule,
@@ -38,9 +59,15 @@ enableProdMode();
     BrowserAnimationsModule,
     MatCardModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatGridListModule,
   ],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}, CookieService],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
